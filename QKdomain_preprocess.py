c5181f67dc3c12f9babd2119edb34ee633458703
matthewmoscou <matthew.moscou@tsl.ac.uk>
2017-02-22 16:42:51 +0000

    Initial commit, updated README

---
+++ QKdomain_preprocess.py
@@ -0,0 +1,68 @@
+#! /usr/bin/python
+
+"""
+%prog interproscan_output preprocess_summary
+Reads InterProScan output and identifies the breadth of domains present within a set of proteins.
+
+Author: Matthew Moscou <matthew.moscou@tsl.ac.uk> and Helen Brabham <helen.brabham@tsl.ac.uk>
+This performs the following:
+   1. Reads InterProScan output
+   2. Identifies the breadth of domains present within a set of proteins
+      This is used for subsequent analyses, allowing Pfam or other domains to be merged
+
+Improvements from existing script set (22 February 2017):
+
+Future improvements to include:
+"""
+
+# modules
+import optparse
+from optparse import OptionParser
+
+import sets
+
+import string
+
+
+# function
+def average(number_list):
+	return float(sum(number_list)) / len(number_list)
+
+
+# import arguments and options
+usage = "usage: %prog interproscan_output preprocess_summary"
+parser = OptionParser(usage=usage)
+(options, args) = parser.parse_args()
+
+
+# import interproscan output
+interproscan_file = open(args[0], 'r')
+
+# initialize gene position domain dictionary
+# domainID -> [software, short name, long name, [geneID.1, geneID.2, ...]]
+domain_software_annotation_frequency_length = {}
+
+for line in interproscan_file.readlines():
+	line = string.replace(line, '\n', '')
+	sline = string.split(line, '\t')
+	
+	if sline[4] not in domain_software_annotation_frequency_length.keys():
+		if len(sline) < 13:
+			domain_software_annotation_frequency_length[sline[4]] = [sline[3], sline[5], '', [], []]
+		else:
+			domain_software_annotation_frequency_length[sline[4]] = [sline[3], sline[5], sline[12], [], []]
+	
+	domain_software_annotation_frequency_length[sline[4]][3].append(sline[0])
+	domain_software_annotation_frequency_length[sline[4]][4].append(float(int(sline[7]) - int(sline[6]) + 1))
+
+interproscan_file.close()
+
+preprocess_summary = open(args[1], 'w')
+
+preprocess_summary.write('domainID' + '\t' + 'software' + '\t' + 'shortname' + '\t' + 'longname' + '\t' + 'genes_with_domain' + '\t' + 'total_observed_domains' + '\t' + 'average_domain_length' + '\n')
+
+for domain in domain_software_annotation_frequency_length.keys():
+	preprocess_summary.write(domain + '\t' + '\t'.join(domain_software_annotation_frequency_length[domain][:3]) + '\t' + str(len(sets.Set(domain_software_annotation_frequency_length[domain][3]))) + '\t' + str(len(domain_software_annotation_frequency_length[domain][3])) + '\t' + str(average(domain_software_annotation_frequency_length[domain][4])) + '\n')
+
+
+preprocess_summary.close()
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # QKdomain
 A set of scripts that can be used for domain analysis. The majority of which are developed to process files for input/output to/from InterProScan, MEME Suite, and phylogenetic analysis.
+
+## Scripts
+<i>QKdomain_preprocess.py</i> reads InterProScan output and identifies the breadth of domains present within a set of proteins. The user can then specify abbreviations and groupings in subsequent analyses.
+<i>QKdomain_extract.py</i> reads protein sequence and InterProScan output, then performs one or more of the following analyses:
+1. Annotate domain composition in all proteins
+2. Extracts protein sequence not associated with a known domain
+3. Extracts protein sequence encompassing specific domains (optional)
+4. Extract proteins with a specific domain composition, strict or relaxed (optional)
+<i>QKdomain_alignment.py</i> assesses the quality of a multiple sequence alignment in representation and coverage.
